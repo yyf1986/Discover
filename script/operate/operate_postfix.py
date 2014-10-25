efbfaa12c956622daa494871aea8703dec4ddfb5
YAO <yyf1986@users.noreply.github.com>
2014-10-25 09:13:37 +0800

    Update operate_postfix.py

--- script/operate/operate_postfix.py
+++ script/operate/operate_postfix.py
@@ -23,19 +23,17 @@ def start_stop_postfix(action,ip,configfile):
             cmd = """ service postfix status """
             out = run(ip,'root',cmd)
             if re.search('is stopped',out):
-                return "%s postfix stop sucess." % ip
+                print "%s postfix stop sucess." % ip
             else:
-                return "%s postfix stop fail." % ip
+                print "%s postfix stop fail." % ip
         elif action == "start":
             cmd = """ service postfix start """
             run(ip,'root',cmd)
             cmd = """ service postfix status """
             out = run(ip,'root',cmd)
             if re.search('is running',out):
-                return "%s postfix start sucess." % ip
+                print "%s postfix start sucess." % ip
             else:
-                return "%s postfix start fail." % ip
+                print "%s postfix start fail." % ip
     else:
-        return ""
-
-#print start_stop_postfix("start","192.168.171.1","/home/13110508/yao/conf/192.168.171.1.txt")
+        pass
