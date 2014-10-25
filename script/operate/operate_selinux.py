110ac8756bcda736bd65f02981c240aedd635867
YAO <yyf1986@users.noreply.github.com>
2014-10-25 09:14:39 +0800

    Update operate_selinux.py

--- script/operate/operate_selinux.py
+++ script/operate/operate_selinux.py
@@ -17,8 +17,6 @@ def start_stop_selinux(action,ip,configfile):
     cmd = """ less %s |grep 'selinux:1'|wc -l """ % configfile
     out = commands.getoutput(cmd)
     if int(out) == 1:
-        return "%s selinux 配置有问题请检查." % ip
+        print "%s selinux 配置有问题请检查." % ip
     else:
-        return ""
-
-#print start_stop_selinux("start","192.168.171.1","/home/13110508/yao/conf/192.168.171.1.txt")
+        pass
