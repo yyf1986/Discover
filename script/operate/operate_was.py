8b375ab9c2dac86be8d15da0ce8da78254ec35ed
YAO <yyf1986@users.noreply.github.com>
2014-10-25 09:20:26 +0800

    Update operate_was.py

--- script/operate/operate_was.py
+++ script/operate/operate_was.py
@@ -3,12 +3,13 @@
 
 import re
 import os
+import commands
 import time
 
 def run(ip,user,cmd):
     cmds = """ sudo ssh -oConnectTimeout=10 %s@%s "%s" """ % (user,ip,cmd)
     try:
-        out = os.popen(cmds).readlines()
+        out = commands.getoutput(cmds)
     except Exception,e:
         out = "error"
     return out
@@ -19,40 +20,74 @@ def start_stop_was(action,ip,configfile):
     if int(out) == 1:
         cmd = """ less %s |grep 'WAS_USER:'|awk -F ':' '{print $2}' """ % configfile
         wasuser = os.popen(cmd).readlines()[0].strip('\r\n')
-
-        cmd = """ less %s |grep 'profile_info:' | wc -l """ % configfile
-        num = os.popen(cmd).readlines()[0].strip('\r\n')
-        if int(num) == 1:
-            cmd = """ less %s |grep 'profile_info:' | awk -F ':' '{print $2}' """ % configfile
-            profile_infos = os.popen(cmd).readlines()[0].strip('\r\n')
-            for profile_info in profile_infos.split(';'):
-                if profile_info != "":
-                    profile = profile_info.split('--')[0]
-                    servers = profile_info.split('--')[1]
-                    for server in servers.split(','):
-                        if action == "start":
-                            cmd = """ su - %s -c "cd %s/bin/;./startServer.sh %s" """ % (wasuser,profile,server)
-                            print cmd
-                        elif action == "stop":
-                            cmd = """ su - %s -c "cd %s/bin/;./stopServer.sh %s -username operator -password xtbspasswd" """ % (wasuser,profile,server)
-                            print cmd
-        else:
-            pass
-
+        
         cmd = """ less %s |grep 'Dmgr_path:'|wc -l """ % configfile
         num = os.popen(cmd).readlines()[0].strip('\r\n')
         if int(num) == 1:
             cmd = """ less %s |grep 'Dmgr_path:'|awk -F ':' '{print $2}' """ % configfile
             dmgr_path = os.popen(cmd).readlines()[0].strip('\r\n')
             if action == "start":
-                cmd = """ su - %s -c "cd %s/bin;./startManager.sh" """ % (wasuser,dmgr_path)
-                print cmd
-            elif action == "stop":
-                cmd = """ su - %s -c "cd %s/bin;./stopManager.sh -username operator -password xtbspasswd" """ % (wasuser,dmgr_path)
-                print cmd
+                cmd = """ su - %s -c 'cd %s/bin;./startManager.sh' """ % (wasuser,dmgr_path)
+                print "begin to start dmgr %s\r\n" % dmgr_path
+                print run(ip,'root',cmd)
+            cmd = """ less %s |grep 'profile_info:' | wc -l """ % configfile
+            num = os.popen(cmd).readlines()[0].strip('\r\n')
+            if int(num) == 1:
+                cmd = """ less %s |grep 'profile_info:' | awk -F ':' '{print $2}' """ % configfile
+                profile_infos = os.popen(cmd).readlines()[0].strip('\r\n')
+                for profile_info in profile_infos.split(';'):
+                    if profile_info != "":
+                        profile = profile_info.split('--')[0]
+                        servers = profile_info.split('--')[1]
+                        if action == "start":
+                            cmd = """ su - %s -c 'cd %s/bin/;./startNode.sh' """ % (wasuser,profile)
+                            print "begin to start node %s\r\n" % profile
+                            print run(ip,'root',cmd)
+                        for server in servers.split(','):
+                            if action == "start":
+                                cmd = """ su - %s -c 'cd %s/bin/;./startServer.sh %s' """ % (wasuser,profile,server)
+                                print "begin to start server %s %s\r\n" % (profile,server)
+                                print run(ip,'root',cmd)
+                            elif action == "stop":
+                                cmd = """ su - %s -c 'cd %s/bin/;./stopServer.sh %s -username operator -password 111111' """ % (wasuser,profile,server)
+                                print "begin to stop server %s %s\r\n" % (profile,server)
+                                print run(ip,'root',cmd)
+                        if action == "stop":
+                            cmd = """ su - %s -c 'cd %s/bin/;./stopNode.sh -username operator -password 111111' """ % (wasuser,profile)
+                            print "begin to stop node %s\r\n" % profile
+                            print run(ip,'root',cmd)
+            else:
+                pass
+            if action == "stop":
+                cmd = """ su - %s -c 'cd %s/bin;./stopManager.sh -username operator -password 111111' """ % (wasuser,dmgr_path)
+                print "begin to stop dmgr %s\r\n" % dmgr_path
+                print run(ip,'root',cmd)
         else:
-            pass
+            cmd = """ less %s |grep 'profile_info:' | wc -l """ % configfile
+            num = os.popen(cmd).readlines()[0].strip('\r\n')
+            if int(num) == 1:
+                cmd = """ less %s |grep 'profile_info:' | awk -F ':' '{print $2}' """ % configfile
+                profile_infos = os.popen(cmd).readlines()[0].strip('\r\n')
+                for profile_info in profile_infos.split(';'):
+                    if profile_info != "":
+                        profile = profile_info.split('--')[0]
+                        servers = profile_info.split('--')[1]
+                        if action == "start":
+                            cmd = """ su - %s -c 'cd %s/bin/;./startNode.sh' """ % (wasuser,profile)
+                            print "begin to start node %s\r\n" % profile
+                            print run(ip,'root',cmd)
+                        for server in servers.split(','):
+                            if action == "start":
+                                cmd = """ su - %s -c 'cd %s/bin/;./startServer.sh %s' """ % (wasuser,profile,server)
+                                print "begin to start server %s %s\r\n" % (profile,server)
+                                print run(ip,'root',cmd)
+                            elif action == "stop":
+                                cmd = """ su - %s -c 'cd %s/bin/;./stopServer.sh %s -username operator -password 111111' """ % (wasuser,profile,server)
+                                print "begin to stop server %s %s\r\n" % (profile,server)
+                                print run(ip,'root',cmd)
+                        if action == "stop":
+                            cmd = """ su - %s -c 'cd %s/bin/;./stopNode.sh -username operator -password 111111' """ % (wasuser,profile)  
+                            print "begin to stop node %s\r\n" % profile
+                            print run(ip,'root',cmd)
     else:
-        return ""
-
-start_stop_was("stop","192.168.161.1","/home/13110508/yao/conf/192.168.161.1.txt")
+        pass
